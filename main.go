5b531c53780ced35eba1e7c4f2946aec1863e508
anchit-nishant <32186023+anchit-nishant@users.noreply.github.com>
2020-03-03 09:50:25 +0530

    change to test cd

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World once again"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
